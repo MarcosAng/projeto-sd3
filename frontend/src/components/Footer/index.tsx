b2479649f448cc4f5bf32847d4964fe5c43dc206
MarcosAng <marcosang@gmail.com>
2021-05-04 21:37:26 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,34 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          App desenvolvido por{" "}
+          <a
+            href="https://www.linkedin.com/in/marcos-rossano-angeloni-163245145/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Marcos Rossano Angeloni
+          </a>
+        </p>
+        <p className="text-light">
+          <small>
+            <strong>Semana Spring React</strong>
+            <br />
+            Evento promovido pela escola DevSuperior:{" "}
+            <a
+              href="https://instagram.com/devsuperior.ig"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @devsuperior.ig
+            </a>
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
