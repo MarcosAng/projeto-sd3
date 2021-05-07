94f4ffa0f4fd53bd63e780c2f3807977f81cdf1f
MarcosAng <marcosang@gmail.com>
2021-05-07 17:41:53 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,19 @@
+import DashBoard from "pages/Dashboard";
+import Home from "pages/home";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/dashboard">
+          <DashBoard />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
